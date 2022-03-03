773776db79148fb0d823a6e1a164cef9d5b68176
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-03-03 16:53:47 +0300

    gitignore added

--- 0_test_tasks.py
+++ 0_test_tasks.py
@@ -1,21 +1,9 @@
-def binary_search(list, item):
-	low = 0
-	high = len(list) - 1
-
-	while low <= high:
-		mid = (low + high) / 2
-		guess = list[mid]
-
-		if guess == item:
-			return mid
-		elif guess > item:
-			high = mid - 1
+def sum(list):
+	if list == 2:
+		if list[0] >= list[1]:
+			return list[0]
 		else:
-			low = mid + 1
+			return list[1]
+	return 1 + sum(list[1:])
 
-	return None
-
-#main
-my_list = [1, 3, 4, 5, 7, 8, 9]
-print(binary_search(my_list, 2))
-print(binary_search(my_list, 3))
+print(sum([1, 2, 4]))
